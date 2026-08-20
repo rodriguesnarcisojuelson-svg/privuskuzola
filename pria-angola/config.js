9d0a88fe34e53aeb29302afcfd07715498256444
rodriguesnarcisojuelson-svg <rodriguesnarcisojuelson@gmail.com>
2026-08-20 11:08:17 +0100

    Update config.js

--- pria-angola/config.js
+++ pria-angola/config.js
@@ -1,5 +1,3 @@
-<script>
 window.PRIA_FORM_ENDPOINT = "/api/report";
 window.PRIA_CAMPAIGN = "PRIA Angola — lançamento gratuito até 03/09/2026";
 window.PRIA_PRICE_AFTER_FREE_PERIOD = "400.000 Kz";
-</script>
